e01e4530a98c3ec4584b1c0dadacc2c8eded9879
Loris Grandchamp <loris.grandchamp@etu.univ-orleans.fr>
2024-02-22 09:32:33 +0100

    Début implémentation Etudiant

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+import java.util.List;
 public class Etudiant {
+    private String prenom;
+    private String nom;
+    private int age;
+    private List<Note> mesNotes;
     
+
+    public Etudiant(String prenom, String nom, int age) {
+        this.prenom = prenom ;
+        this.nom = nom;
+        this.age = age;
+        this.mesNotes = new ArrayList<>();
+    }
 }
