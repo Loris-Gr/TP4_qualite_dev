9eed8b2a45140327214fba67731d9cad356fae6c
Loris Grandchamp <loris.grandchamp@etu.univ-orleans.fr>
2024-02-22 09:22:46 +0100

    Implémentation bases de Note

--- src/Matiere.java
+++ src/Matiere.java
@@ -23,6 +23,4 @@ public enum Matiere {
             return "Bash";
         }
     }
-
-
 }
--- src/Note.java
+++ src/Note.java
@@ -1,3 +1,17 @@
 public class Note {
-    
+    private String nomControle ;
+    private int resultat ;
+
+    public Note(int resultat, String nomControle) {
+        this.resultat = resultat ;
+        this.nomControle = nomControle ;
+    }
+
+    public String getNomControle() {
+        return this.nomControle;
+    }
+
+    public int getResultat() {
+        return this.resultat;
+    }
 }
