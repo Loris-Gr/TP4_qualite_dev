e5637e7e224b47954549238b6fa63dc65ba86035
Loris Grandchamp <loris.grandchamp@etu.univ-orleans.fr>
2024-03-14 08:24:28 +0100

    re-ajout travail dernier

--- src/Groupe.java
+++ src/Groupe.java
@@ -1,3 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
 public class Groupe {
-    
+    private String nomGroupe;
+    private List<Etudiant> lesEtudiants;
+
+    public Groupe(String nomGroupe) {
+        this.nomGroupe = nomGroupe ;
+        this.lesEtudiants = new ArrayList<>();
+    } 
+
+    public Groupe(String nomGroupe, List<Etudiant> lesEtudiants) {
+        this.nomGroupe = nomGroupe ;
+        this.lesEtudiants = lesEtudiants;
+    }
+
+    public String getNom() {
+        return this.nomGroupe ;
+    }
+
+    public List<Etudiant> getEtudiants() {
+        return this.lesEtudiants;
+    }
 }
